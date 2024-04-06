908cc697aeaa876cf52f1b6173f5e101b9b9746f
zhedlund <zhedlund@student.42berlin.de>
2024-04-06 18:53:20 +0200

    variable names

--- testing/rt1wkd/rt1wkd.c
+++ testing/rt1wkd/rt1wkd.c
@@ -7,7 +7,6 @@
 
 typedef struct
 {
-    double e[3]; // x, y, z
 	double x;
 	double y;
 	double z;
@@ -31,17 +30,17 @@ typedef struct
 
 double dot(const vec3 *u, const vec3 *v) 
 {
-    return (u->e[0] * v->e[0] + u->e[1] * v->e[1] + u->e[2] * v->e[2]);
+    return (u->x * v->x + u->y * v->y + u->z * v->z);
 }
 
 double vec3_length_squared(const vec3 *v)
 {
-    return (v->e[0] * v->e[0] + v->e[1] * v->e[1] + v->e[2] * v->e[2]);
+    return (v->x * v->x + v->y * v->y + v->z * v->z);
 }
 
 double hit_sphere(const point3 *center, double radius, const ray *r)
 {
-    vec3 oc = {r->orig.e[0] - center->e[0], r->orig.e[1] - center->e[1], r->orig.e[2] - center->e[2] };
+    vec3 oc = {r->orig.x - center->x, r->orig.y - center->y, r->orig.z - center->z };
     double a = vec3_length_squared(&r->dir);
     double half_b = dot(&oc, &r->dir);
     double c = vec3_length_squared(&oc) - radius * radius;
@@ -57,9 +56,9 @@ double hit_sphere(const point3 *center, double radius, const ray *r)
 void write_color(color pixel_color) 
 {
     printf("%d %d %d\n",
-           (int)(255.999 * pixel_color.e[0]),
-           (int)(255.999 * pixel_color.e[1]),
-           (int)(255.999 * pixel_color.e[2]));
+           (int)(255.999 * pixel_color.x),
+           (int)(255.999 * pixel_color.y),
+           (int)(255.999 * pixel_color.z));
 }
 
 color ray_color(const ray *r)
@@ -70,21 +69,21 @@ color ray_color(const ray *r)
 
     if (t > 0.0)
 	{
-        vec3 n = { r->orig.e[0] + t * r->dir.e[0], r->orig.e[1] + t * r->dir.e[1], r->orig.e[2] + t * r->dir.e[2] };
+        vec3 n = { r->orig.x + t * r->dir.x, r->orig.y + t * r->dir.y, r->orig.z + t * r->dir.z };
         vec3 temp = { 0, 0, -1 };
-        vec3 unit_n = { (n.e[0] - temp.e[0]) / radius, (n.e[1] - temp.e[1]) / radius, (n.e[2] - temp.e[2]) / radius };
-        color res = { 0.5 * (unit_n.e[0] + 1), 0.5 * (unit_n.e[1] + 1), 0.5 * (unit_n.e[2] + 1) };
-        return res;
+        vec3 unit_n = { (n.x - temp.x) / radius, (n.y - temp.y) / radius, (n.z - temp.z) / radius };
+        color res = { 0.5 * (unit_n.x + 1), 0.5 * (unit_n.y + 1), 0.5 * (unit_n.z + 1) };
+        return (res);
     }
-    vec3 unit_direction = { r->dir.e[0] / sqrt(dot(&r->dir, &r->dir)),
-                             r->dir.e[1] / sqrt(dot(&r->dir, &r->dir)),
-                             r->dir.e[2] / sqrt(dot(&r->dir, &r->dir)) };
-    double a = 0.5 * (unit_direction.e[1] + 1.0);
+    vec3 unit_direction = { r->dir.x / sqrt(dot(&r->dir, &r->dir)),
+                             r->dir.y / sqrt(dot(&r->dir, &r->dir)),
+                             r->dir.z / sqrt(dot(&r->dir, &r->dir)) };
+    double a = 0.5 * (unit_direction.y + 1.0);
     color white = { 1.0, 1.0, 1.0 };
     color blue = { 0.5, 0.7, 1.0 };
-    color term1 = { (1.0 - a) * white.e[0], (1.0 - a) * white.e[1], (1.0 - a) * white.e[2] };
-    color term2 = { a * blue.e[0], a * blue.e[1], a * blue.e[2] };
-    color res = { term1.e[0] + term2.e[0], term1.e[1] + term2.e[1], term1.e[2] + term2.e[2] };
+    color term1 = { (1.0 - a) * white.x, (1.0 - a) * white.y, (1.0 - a) * white.z };
+    color term2 = { a * blue.x, a * blue.y, a * blue.z };
+    color res = { term1.x + term2.x, term1.y + term2.y, term1.z + term2.z };
     return (res);
 }
 
@@ -108,16 +107,16 @@ int main()
     vec3 viewport_v = {0, -viewport_height, 0};
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    vec3 pixel_delta_u = {viewport_u.e[0] / image_width, 0, 0};
-    vec3 pixel_delta_v = {0, viewport_v.e[1] / image_height, 0};
+    vec3 pixel_delta_u = {viewport_u.x / image_width, 0, 0};
+    vec3 pixel_delta_v = {0, viewport_v.y / image_height, 0};
 
     // Calculate the location of the upper left pixel.
-    vec3 viewport_up_left = {camera_center.e[0] - 0.5 * viewport_u.e[0] - 0.5 * viewport_v.e[0],
-                                camera_center.e[1] - 0.5 * viewport_u.e[1] - 0.5 * viewport_v.e[1],
-                                camera_center.e[2] - focal_length};
-    vec3 pixel00_loc = {viewport_up_left.e[0] + 0.5 * (pixel_delta_u.e[0] + pixel_delta_v.e[0]),
-                        viewport_up_left.e[1] + 0.5 * (pixel_delta_u.e[1] + pixel_delta_v.e[1]),
-                        viewport_up_left.e[2]};
+    vec3 viewport_up_left = {camera_center.x - 0.5 * viewport_u.x - 0.5 * viewport_v.x,
+                                camera_center.y - 0.5 * viewport_u.y - 0.5 * viewport_v.y,
+                                camera_center.z - focal_length};
+    vec3 pixel00_loc = {viewport_up_left.x + 0.5 * (pixel_delta_u.x + pixel_delta_v.x),
+                        viewport_up_left.y + 0.5 * (pixel_delta_u.y + pixel_delta_v.y),
+                        viewport_up_left.z};
 
 	// Render
     printf("P3\n%d %d\n255\n", image_width, image_height);
@@ -127,8 +126,8 @@ int main()
     	int i = 0;
     	while (i < image_width)
 		{
-        	vec3 pixel_center = {pixel00_loc.e[0] + i * pixel_delta_u.e[0], pixel00_loc.e[1] + j * pixel_delta_v.e[1], pixel00_loc.e[2]};
-        	vec3 ray_direction = {pixel_center.e[0] - camera_center.e[0], pixel_center.e[1] - camera_center.e[1], pixel_center.e[2] - camera_center.e[2]};
+        	vec3 pixel_center = {pixel00_loc.x + i * pixel_delta_u.x, pixel00_loc.y + j * pixel_delta_v.y, pixel00_loc.z};
+        	vec3 ray_direction = {pixel_center.x - camera_center.x, pixel_center.y - camera_center.y, pixel_center.z - camera_center.z};
         	ray r = {camera_center, ray_direction};
 
         	color pixel_color = ray_color(&r);
